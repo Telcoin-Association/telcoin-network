7d727e0c64b77d687b7b2bc066da9a580c6db40a
grantkee <grant@kee.ai>
2024-05-31 18:14:07 -0500

    pass block executor in main

--- bin/telcoin-network/src/main.rs
+++ bin/telcoin-network/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser as _;
 #[cfg(not(feature = "faucet"))]
 use reth::commands::node::NoArgs;
-use reth_node_ethereum::EthEvmConfig;
+use reth_node_ethereum::{EthEvmConfig, EthExecutorProvider};
 #[cfg(feature = "faucet")]
 use tn_faucet::FaucetArgs;
 use tn_node::launch_node;
@@ -15,7 +15,8 @@ fn main() {
     #[cfg(not(feature = "faucet"))]
     if let Err(err) =
         telcoin_network::cli::Cli::<NoArgs>::parse().run(|builder, _, tn_datadir| async move {
-            launch_node(builder, EthEvmConfig::default(), tn_datadir).await
+            let executor = EthExecutorProvider::new(std::sync::Arc::clone(&builder.node_config.chain), EthEvmConfig::default());
+            launch_node(builder, executor, tn_datadir).await
         })
     {
         eprintln!("Error: {err:?}");
@@ -26,7 +27,8 @@ fn main() {
     if let Err(err) = telcoin_network::cli::Cli::<FaucetArgs>::parse().run(
         |mut builder, faucet, tn_datadir| async move {
             builder.opt_faucet_args = Some(faucet);
-            launch_node(builder, EthEvmConfig::default(), tn_datadir).await
+            let executor = EthExecutorProvider::new(std::sync::Arc::clone(&builder.node_config.chain), EthEvmConfig::default());
+            launch_node(builder, executor, tn_datadir).await
         },
     ) {
         eprintln!("Error: {err:?}");
