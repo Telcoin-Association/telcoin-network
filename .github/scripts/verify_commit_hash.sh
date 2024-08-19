749392840688cf4b9a21409e4b2e550e6cb68b91
grantkee <grant@kee.ai>
2024-08-19 12:23:03 -0600

    remove chain id arg altogether

--- .github/scripts/verify_commit_hash.sh
+++ .github/scripts/verify_commit_hash.sh
@@ -13,13 +13,12 @@ COMMIT_HASH=$(git rev-parse HEAD)
 # NOTE: this contract must match local test-and-attest.sh
 CONTRACT_ADDRESS="0x1f2f25561a11762bdffd91014c6d0e49af334447"
 RPC_ENDPOINT="https://rpc.adiri.tel"
-CHAIN_ID=2017
 
 # Function call
 FUNCTION_CALL="gitCommitHashAttested(bytes20)"
 
 # Use cast to call the contract
-RESULT=$(cast call --rpc-url ${RPC_ENDPOINT} --chain ${CHAIN_ID} \
+RESULT=$(cast call --rpc-url ${RPC_ENDPOINT} \
     ${CONTRACT_ADDRESS} "${FUNCTION_CALL}" "${COMMIT_HASH}")
 
 # Check if the result is true (1) or false (0)
