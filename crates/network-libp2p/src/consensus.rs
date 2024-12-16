95b180592e10fd3a92f17934429217fa8ad20407
grantkee <grant@kee.ai>
2024-12-15 22:19:16 -0600

    init consensus network struct

---
+++ crates/network-libp2p/src/consensus.rs
@@ -0,0 +1,15 @@
+//! Consensus p2p network.
+//!
+//! This network is used by workers and primaries to reliably send consensus messages.
+
+use libp2p::Swarm;
+use tokio::sync::mpsc::{Receiver, Sender};
+
+pub struct ConsensusNetwork {
+    /// The gossip network for flood publishing sealed worker blocks.
+    swarm: Swarm<request_response::Behaviour>,
+    /// The sender for network handles.
+    handle: Sender<()>,
+    /// The receiver for processing network handle requests.
+    commands: Receiver<()>,
+}
--- crates/network-libp2p/src/lib.rs
+++ crates/network-libp2p/src/lib.rs
@@ -1,5 +1,6 @@
 //! Peer-to-peer network interface for Telcoin Network built using libp2p.
 
+mod consensus;
 pub mod error;
 mod helpers;
 mod publish;
