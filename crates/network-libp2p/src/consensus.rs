19d0baa89328e1a6938de5e86a9256261011562e
grantkee <grant@kee.ai>
2024-12-19 20:47:30 +0100

    test passing

--- crates/network-libp2p/src/consensus.rs
+++ crates/network-libp2p/src/consensus.rs
@@ -10,7 +10,7 @@ use libp2p::{
     request_response::{self, Codec},
     StreamProtocol, Swarm,
 };
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Serialize};
 use snap::read::FrameDecoder;
 use std::{
     io::{Read as _, Write as _},
@@ -18,7 +18,6 @@ use std::{
 };
 use tn_types::encode;
 use tokio::sync::mpsc::{Receiver, Sender};
-use tokio_util::codec::{Decoder, Encoder};
 
 pub struct ConsensusNetwork<C: Codec + Send + Clone + 'static> {
     /// The gossip network for flood publishing sealed worker blocks.
@@ -34,9 +33,9 @@ pub struct TNCodec<T, U> {
     /// The max possible compression length based on the max uncompressed message size.
     max_compress_len: u64,
     /// The fixed-size buffer for compressed messages.
-    compressed_buffer: BytesMut,
+    compressed_buffer: Vec<u8>,
     /// The fixed-size buffer for requests.
-    request_buffer: BytesMut,
+    decode_buffer: Vec<u8>,
     /// The fixed-size buffer for responses.
     response_buffer: BytesMut,
     /// The maximum size (bytes) in a chunked request/response.
@@ -50,16 +49,16 @@ impl<T, U> TNCodec<T, U> {
     pub fn new(max_chunk_size: usize) -> Self {
         // create buffer from max possible compression length based on max request size
         let max_compress_len = snap::raw::max_compress_len(MAX_REQUEST_SIZE);
-        let compressed_buffer = BytesMut::with_capacity(max_compress_len);
+        let compressed_buffer = Vec::with_capacity(max_compress_len);
 
-        let request_buffer = BytesMut::with_capacity(MAX_REQUEST_SIZE);
+        let decode_buffer = Vec::with_capacity(MAX_REQUEST_SIZE);
         let response_buffer = BytesMut::with_capacity(MAX_RESPONSE_SIZE);
 
-        // NOTE: usize -> u64 won't lose precision (even on 32bit systems)
         Self {
+            // NOTE: usize -> u64 won't lose precision (even on 32bit systems)
             max_compress_len: max_compress_len as u64,
             compressed_buffer,
-            request_buffer,
+            decode_buffer,
             response_buffer,
             max_chunk_size,
             _phantom: PhantomData::<(T, U)>,
@@ -97,99 +96,44 @@ where
     where
         T: AsyncRead + Unpin + Send,
     {
-        // allocate reusable buffer on `self`
-        // set max to 1mb uncompressed (worker block)
-        // clear reusable buffer at the beginning
-        // take + read_to_end
-        // ditch the prefix
-        // async read bytes
-        //
-
-        // self.fixed_buffer.
-
-        // let mut compressed = Vec::new();
-        // io.take(MAX_COMPRESSED_REQUEST_SIZE).read_to_end(&mut compressed).await?;
-
         // clear buffers
         self.compressed_buffer.clear();
-        self.request_buffer.clear();
-        // let compressed_size = self.request_buffer.remaining();
-        // println!("remaining after clear: {compressed_size:?}");
-
-        let comp_length = self.compressed_buffer.len();
-        println!("compressed len before: {comp_length:?}");
+        self.decode_buffer.clear();
 
         // retrieve prefix for uncompressed message length
         let mut prefix = [0; 4];
         io.read_exact(&mut prefix).await?;
 
-        // NOTE: cast usize to u32 safe
+        // NOTE: cast usize to u32 is safe
         let length = u32::from_le_bytes(prefix) as usize;
 
-        println!("prefix: {prefix:?}");
+        // ensure message length within bounds
+        if length > MAX_REQUEST_SIZE {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "prefix indicates message size is too large",
+            ));
+        }
 
-        // take max request size so request buffer doesn't reallocate
-        let mut vec = Vec::new();
-        io.take(self.max_compress_len).read_to_end(&mut vec).await?;
-        println!("vec?:\n{vec:?}");
-
-        // NOTE: this returns early with 0 bytes - no guarantees this reads all bytes,
-        // so try using read_exact or read_to_end:
+        // resize buffer to reported message size
         //
-        // io.take(self.max_compress_len).read(&mut self.compressed_buffer).await?;
-        // println!("compressed_buffer?:\n{:?}", self.compressed_buffer);
+        // NOTE: this should not reallocate
+        self.decode_buffer.resize(length, 0);
 
-        // let compressed_size = self.request_buffer.remaining();
-        // println!("remaining after read?? {compressed_size:?}");
-
-        // if compressed_size == 0 {
-        //     // TODO: should this be an error?
-        //     println!("ZERO BYTES!!!");
-        // }
+        // take max possible compression size based on reported length
+        //
+        // NOTE: usize -> u64 won't lose precision (even on 32bit system)
+        let max_compress_len = snap::raw::max_compress_len(length);
+        io.take(max_compress_len as u64).read_to_end(&mut self.compressed_buffer).await?;
 
         // decompress bytes
-        // read message up to the max possible compression length
-        let length = self.request_buffer.len();
-        println!("request buffer length before: {length:?}");
-        println!("max compress len: {:?}", self.max_compress_len);
+        let reader = std::io::Cursor::new(&mut self.compressed_buffer);
+        let mut snappy_decoder = FrameDecoder::new(reader);
+        snappy_decoder.read_exact(&mut self.decode_buffer)?;
 
-        let comp_length = self.compressed_buffer.len();
-        println!("compressed len after read: {comp_length:?}");
-        println!("compressed_buffer:\n{:?}", self.compressed_buffer);
-        println!("request_buffer\n{:?}", self.request_buffer);
-
-        // determine max compression length possible based on msg prefix
-        //
-        // NOTE: usize -> u64 won't lose precision (even on 32)
-        // let max_compressed_len = snap::raw::max_compress_len(length) as u64;
-
-        let length = snap::raw::max_compress_len(vec.len());
-        let reader_limited = std::io::Cursor::new(vec); //.take(self.max_compress_len);
-                                                        // std::io::Cursor::new(self.compressed_buffer.as_ref()).take(self.max_compress_len);
-        let mut snappy_decoder = FrameDecoder::new(reader_limited);
-
-        let mut request_buffer = vec![0; length];
-        snappy_decoder.read_to_end(&mut request_buffer)?;
-
-        bcs::from_bytes(&self.request_buffer)
+        // decode bytes
+        bcs::from_bytes(&self.decode_buffer)
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-
-        // // determine max compression length possible based on msg prefix
-        // //
-        // // NOTE: usize -> u64 won't lose precision
-        // let max_compressed_len = snap::raw::max_compress_len(prefix) as u64;
-
-        // // read message up to the max possible compression length
-        // let limited_reader = std::io::Cursor::new(&compressed).take(max_compressed_len);
-        // let mut reader = snap::read::FrameDecoder::new(limited_reader);
-        // let mut decoded_buffer = vec![0; prefix];
-
-        // reader.read_exact(&mut decoded_buffer)?;
-        // let req = Self::Request::from_ssz_bytes(&decoded_buffer[..]).map_err(|_| {
-        //     std::io::Error::new(std::io::ErrorKind::Other, "failed to cast request from ssz bytes")
-        // })?;
-        // Ok(req)
-        // todo!()
     }
 
     async fn read_response<T>(
@@ -200,22 +144,44 @@ where
     where
         T: AsyncRead + Unpin + Send,
     {
-        // async read bytes
-        let mut compressed = Vec::new();
-        // io.take(MAX_COMPRESSED_REQUEST_SIZE).read_to_end(&mut compressed).await?;
+        // clear buffers
+        self.compressed_buffer.clear();
+        self.decode_buffer.clear();
 
-        // spawn blocking task
+        // retrieve prefix for uncompressed message length
+        let mut prefix = [0; 4];
+        io.read_exact(&mut prefix).await?;
+
+        // NOTE: cast usize to u32 is safe
+        let length = u32::from_le_bytes(prefix) as usize;
+
+        // ensure message length within bounds
+        if length > MAX_REQUEST_SIZE {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "prefix indicates message size is too large",
+            ));
+        }
+
+        // resize buffer to reported message size
         //
-        // NOTE: io limited by max allowable size
-        tokio::task::spawn_blocking(move || {
-            let mut decoder = snap::read::FrameDecoder::new(&compressed[..]);
-            let mut uncompressed = Vec::new();
-            std::io::Read::read_to_end(&mut decoder, &mut uncompressed)?;
-            bcs::from_bytes(&uncompressed)
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-        })
-        .await
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?
+        // NOTE: this should not reallocate
+        self.decode_buffer.resize(length, 0);
+
+        // take max possible compression size based on reported length
+        //
+        // NOTE: usize -> u64 won't lose precision (even on 32bit system)
+        let max_compress_len = snap::raw::max_compress_len(length);
+        io.take(max_compress_len as u64).read_to_end(&mut self.compressed_buffer).await?;
+
+        // decompress bytes
+        let reader = std::io::Cursor::new(&mut self.compressed_buffer);
+        let mut snappy_decoder = FrameDecoder::new(reader);
+        snappy_decoder.read_exact(&mut self.decode_buffer)?;
+
+        // decode bytes
+        bcs::from_bytes(&self.decode_buffer)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
     }
 
     async fn write_request<T>(
@@ -227,10 +193,10 @@ where
     where
         T: AsyncWrite + Unpin + Send,
     {
-        // let bytes = req.as_ssz_bytes();
+        // global encode
         let bytes = encode(&req);
 
-        // encoded bytes must be within `max_chunk_size`
+        // ensure encoded bytes are within bounds
         if bytes.len() > self.max_chunk_size {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
@@ -242,13 +208,7 @@ where
         //
         // NOTE: 32bit max 4,294,967,295
         let prefix = (bytes.len() as u32).to_le_bytes();
-
-        println!("prefix: {prefix:?}");
-
         io.write_all(&prefix).await?;
-
-        // encode uncompressed bytes length prefix using unsigned varint
-        // self.prefix.encode(bytes.len(), &mut out)?;
 
         // compress data
         let mut encoder = snap::write::FrameEncoder::new(Vec::new());
@@ -269,10 +229,10 @@ where
     where
         T: AsyncWrite + Unpin + Send,
     {
-        // let bytes = res.as_ssz_bytes();
-        let bytes = Vec::new();
+        // global encode
+        let bytes = encode(&res);
 
-        // encoded bytes must be within `max_chunk_size`
+        // ensure encoded bytes are within bounds
         if bytes.len() > self.max_chunk_size {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
@@ -280,20 +240,19 @@ where
             ));
         }
 
-        let mut out = BytesMut::new();
-
-        // encode uncompressed bytes length prefix using unsigned varint
-        // self.prefix.encode(bytes.len(), &mut out)?;
+        // length prefix for uncompressed bytes
+        //
+        // NOTE: 32bit max 4,294,967,295
+        let prefix = (bytes.len() as u32).to_le_bytes();
+        io.write_all(&prefix).await?;
 
         // compress data
-        let mut writer = snap::write::FrameEncoder::new(Vec::new());
-        writer.write_all(&bytes)?;
-        writer.flush()?;
+        let mut encoder = snap::write::FrameEncoder::new(Vec::new());
+        encoder.write_all(&bytes)?;
+        encoder.flush()?;
 
         // add compressed bytes to prefix
-        out.extend_from_slice(writer.get_ref());
-
-        io.write_all(out.as_ref()).await?;
+        io.write_all(encoder.get_ref()).await?;
         Ok(())
     }
 }
@@ -301,6 +260,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::Deserialize;
     use tn_types::{BlockHash, WorkerBlock};
 
     #[derive(Serialize, Deserialize, Default, PartialEq, Clone, Debug)]
