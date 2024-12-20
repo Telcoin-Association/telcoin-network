32f525ce6f48503a56b6a200372a43c4bf01e882
grantkee <grant@kee.ai>
2024-12-20 14:55:53 +0100

    move codec into separate mod

--- crates/network-libp2p/src/lib.rs
+++ crates/network-libp2p/src/lib.rs
@@ -1,10 +1,10 @@
 //! Peer-to-peer network interface for Telcoin Network built using libp2p.
 
+mod codec;
 mod consensus;
 pub mod error;
 mod helpers;
 mod publish;
-// mod rpc;
 mod subscribe;
 pub mod types;
 pub use publish::*;
