b22c7e47f1d570fc531c15eb8132a4965c1bdd27
grantkee <grant@kee.ai>
2025-04-24 11:27:31 -0600

    epoch manager struct

---
+++ crates/consensus/epoch-manager/src/manager.rs
@@ -0,0 +1,11 @@
+//! The epoch manager type.
+
+/// The long-running type that oversees epoch transitions.
+pub struct EpochManager;
+
+impl EpochManager {
+    /// Create a new instance of [Self].
+    pub fn new() -> Self {
+        Self
+    }
+}
