ec94e11f7e7cc9336d2fe0dfd8c9200451e68390
grantkee <grant@kee.ai>
2025-04-30 18:26:02 -0500

    update task managers for epoch to use const names

--- crates/types/src/task_manager.rs
+++ crates/types/src/task_manager.rs
@@ -308,6 +308,7 @@ impl Display for TaskManager {
         Ok(())
     }
 }
+
 impl Debug for TaskManager {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Display::fmt(self, f)
