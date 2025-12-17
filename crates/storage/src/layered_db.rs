2f0b5f3fb8146686c8720e5d8dab12ca586a4234
Steven Stanfield <steve@telcoin.org>
2025-12-17 18:36:11 -0500

    Fix possible error in layered db is_empty().

--- crates/storage/src/layered_db.rs
+++ crates/storage/src/layered_db.rs
@@ -294,7 +294,11 @@ impl<DB: Database> Database for LayeredDatabase<DB> {
     }
 
     fn is_empty<T: Table>(&self) -> bool {
-        self.mem_db.is_empty::<T>() && self.db.is_empty::<T>()
+        if self.full_memory {
+            self.mem_db.is_empty::<T>()
+        } else {
+            self.mem_db.is_empty::<T>() && self.db.is_empty::<T>()
+        }
     }
 
     /// Layered db will return all inserted elements even if some are duplicates while writes occur.
