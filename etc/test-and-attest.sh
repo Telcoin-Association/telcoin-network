ec60ac084f4a58b407a5b1d08a64dd55d924b089
grantkee <grant@kee.ai>
2026-01-25 14:23:22 -0500

    update attestation contract address

--- etc/test-and-attest.sh
+++ etc/test-and-attest.sh
@@ -21,7 +21,7 @@ if [ -z "$GITHUB_ATTESTATION_PRIVATE_KEY" ]; then
 fi
 
 # NOTE: this contract must match CI
-CONTRACT_ADDRESS="0xba26a2c8e1c54d5bbb96e891fd4a9d853f438eb9"
+CONTRACT_ADDRESS="0xfb8062c74148c4763238b7a46a1b1025470f1160"
 RPC_ENDPOINT="https://rpc.adiri.tel"
 ATTEST_CALL="attestGitCommitHash(bytes20,bool)"
 VERIFY_CALL="gitCommitHashAttested(bytes20)"
