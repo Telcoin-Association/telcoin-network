0aa4af5b0b9a47c810f9658d6b781a41ccd65a10
Steven Stanfield <steve@telcoin.org>
2026-05-27 18:21:14 -0400

    Increase the max size of the cache DB.

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -146,7 +146,7 @@ fn _open_mdbx<P: AsRef<std::path::Path> + Send>(store_path: P) -> CompositeDatab
         } else {
             const EPOCH_MAX: usize = 128 * MEGABYTE;
             const KAD_MAX: usize = 64 * MEGABYTE;
-            const CACHE_MAX: usize = 512 * MEGABYTE;
+            const CACHE_MAX: usize = 1024 * MEGABYTE;
             const GROWTH: usize = 8 * MEGABYTE;
             const CACHE_GROWTH: usize = 64 * MEGABYTE;
         }
