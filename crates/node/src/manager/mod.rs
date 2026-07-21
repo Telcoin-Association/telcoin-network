0c55f2f9985c039e7ccc18653cac193f9db478ea
Steven Stanfield <steve@telcoin.org>
2026-07-21 14:41:09 -0400

    Code to start a thread to do exports.

--- crates/node/src/manager/mod.rs
+++ crates/node/src/manager/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod epoch_votes;
 mod exex;
+pub(crate) mod export_exec;
 mod node;
 
+pub use export_exec::{ExecStateExporter, ExportOutcome};
 pub use node::*;
 
 pub(crate) use epoch_votes::spawn_epoch_vote_collector;
